082a4208e3b6c2a1dc0dec38a071f37333077031
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-31 03:59:30 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,29 +1,29 @@
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  let iconUrl = info.srcUrl;
-  let s = iconUrl.slice(-4);
-  if (s == "=s60" || s == "s120") {
-    iconUrl = iconUrl.slice(0, (s == "=s60") - 3) + ((await chrome.system.display.getInfo())[0].dpiY >> 2);
-    let { title, url: tabUrl } = tab;
-    title =  title.slice(0, title.indexOf(" - Chrome ", 1));
-    let fr = new FileReader;
-    fr.onload = e => {
-      iconUrl = e.target.result;
-      let key = tabUrl.substr(tabUrl.indexOf("/", 43) + 1,  32);
-      chrome.storage.local.get(v => {
-        let i = 0;
-        let j;
-        let hole = null;
-        while (i in v && ((j = v[i][0]) ? j != key : (hole ??= i, 1)))
-          ++i;
-        chrome.storage.local.set({
-          [i in v && hole != null ? hole : i]: [key, title, tabUrl, iconUrl]
+  try {
+    let iconUrl = info.srcUrl;
+    let s = iconUrl.slice(-4);
+    if (s == "=s60" || s == "s120") {
+      iconUrl = iconUrl.slice(0, (s == "=s60") - 3) + ((await chrome.system.display.getInfo())[0].dpiY >> 2);
+      let tabUrl = tab.url;
+      let title = tab.title.replace(/ - Chrome .*/, "");
+      let fr = new FileReader;
+      fr.onload = e => {
+        iconUrl = e.target.result;
+        let key = tabUrl.substr(tabUrl.indexOf("/", 43) + 1,  32);
+        chrome.storage.local.get(v => {
+          let hole = null;
+          let j;
+          let i = 0;
+          while (i in v && ((j = v[i][0]) ? j != key : (hole ??= i, 1)))
+            ++i;
+          chrome.storage.local.set({
+            [i in v && hole != null ? hole : i]: [key, title, tabUrl, iconUrl]
+          });
         });
-      });
-    }
-    try {
+      }
       fr.readAsDataURL(await (await fetch(iconUrl)).blob());
-    } catch {}
-  }
+    }
+  } catch {}
 });
 chrome.runtime.onStartup.addListener(() =>
   chrome.sidePanel.setOptions({
