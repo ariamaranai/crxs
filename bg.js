9340f53d106905ea1eb697533bef896f70a8ccff
a <106607480+ariamaranai@users.noreply.github.com>
2026-05-17 18:08:57 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -2,27 +2,27 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   try {
     let iconUrl = info.srcUrl;
     let is60 = iconUrl.endsWith("=s60");
-    if (is60 || iconUrl.endsWith("s120")) {
-      iconUrl = iconUrl.slice(0, is60 - 3) + ((await chrome.system.display.getInfo())[0].dpiY >>> 2);
-      let tabUrl = tab.url;
-      let title = tab.title.replace(/ - Chrome .*/, "");
-      let fr = new FileReader;
-      fr.onload = e => {
-        iconUrl = e.target.result;
-        let key = tabUrl.substr(tabUrl.indexOf("/", 43) + 1,  32);
-        chrome.storage.local.get(v => {
-          let hole = null;
-          let j;
-          let i = 0;
-          while (i in v && ((j = v[i][0]) ? j != key : (hole ??= i, 1)))
-            ++i;
-          chrome.storage.local.set({
-            [i in v && hole != null ? hole : i]: [key, title, tabUrl, iconUrl]
-          });
+    if (!is60 && !iconUrl.endsWith("s120"))
+      return 0;
+    iconUrl = iconUrl.slice(0, is60 - 3) + ((await chrome.system.display.getInfo())[0].dpiY >>> 2);
+    let tabUrl = tab.url;
+    let title = tab.title.replace(/ - Chrome .*/, "");
+    let fr = new FileReader;
+    fr.onload = e => {
+      iconUrl = e.target.result;
+      let key = tabUrl.substr(tabUrl.indexOf("/", 43) + 1,  32);
+      chrome.storage.local.get(v => {
+        let hole = null;
+        let j;
+        let i = 0;
+        while (i in v && ((j = v[i][0]) ? j != key : (hole ??= i, 1)))
+          ++i;
+        chrome.storage.local.set({
+          [i in v && hole != null ? hole : i]: [key, title, tabUrl, iconUrl]
         });
-      }
-      fr.readAsDataURL(await (await fetch(iconUrl)).blob());
+      });
     }
+    return fr.readAsDataURL(await (await fetch(iconUrl)).blob());
   } catch {}
 });
 chrome.runtime.onInstalled.addListener(() => (
