bbf5882450892194eeb8fd70242e64af8e195456
a <106607480+ariamaranai@users.noreply.github.com>
2026-05-22 02:29:56 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -2,7 +2,7 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   try {
     let iconUrl = info.srcUrl;
     let is60 = iconUrl.endsWith("=s60");
-    if (!is60 && !iconUrl.endsWith("s120"))
+    if (is60 || iconUrl.endsWith("s120"))
       return 0;
     iconUrl = iconUrl.slice(0, is60 - 3) + ((await chrome.system.display.getInfo())[0].dpiY >>> 2);
     let tabUrl = tab.url;
