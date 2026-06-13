e5d4521413ae7eb04d8c4f12a19bc5c596454295
a <106607480+ariamaranai@users.noreply.github.com>
2026-06-13 12:28:36 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 chrome.storage.local.get(v => {
-  let _a = document.createElement("a");
+  let d = document;
+  let _a = d.createElement("a");
   _a.append(new Image(24, 24), "");
   let i = 0;
   while (i in v) {
@@ -8,8 +9,8 @@ chrome.storage.local.get(v => {
       let a = _a.cloneNode(1);
       a.lastChild.data = crx[1];
       a.href = crx[2];
-      a.i = i;
-      document.body.appendChild(a).firstChild.src = crx[3];
+      a[i] = i;
+      d.body.appendChild(a).firstChild.src = crx[3];
     }
     ++i;
   }
@@ -19,7 +20,7 @@ onclick = e => {
   let { target } = e;
   return target.localName == "a" && (
     innerWidth - e.x < 20
-      ? (target.remove(chrome.storage.local.set({ [target.i] : [0] })), document.links.length || close())
+      ? (target.remove(chrome.storage.local.set({ [target[i]] : [0] })), d.links.length || close())
       : chrome.tabs.query({ currentWindow: !0, active: !0 }, tabs =>
           chrome.tabs[tabs[0].url != "chrome://newtab" ? "create": "update"]({ url: target.href })
         )
